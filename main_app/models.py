b6747a4cf84b4c393181eb8105991b202b389a03
gabriel.ramos731 <ramos.gabrielsilva23@gmail.com>
2024-12-04 14:56:25 -0300

    fix: add user atribute on group

--- main_app/models.py
+++ main_app/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 class Grupo(models.Model):
     nome = models.CharField(max_length=50)
     codigo = models.CharField(max_length=6, null=True)
+    admin = models.ForeignKey(get_user_model(), 
+                              on_delete=models.CASCADE,
+                              null=True)
 
     def __str__(self):
         return self.nome
     
-    
 class Participante(models.Model):
     nome = models.CharField(max_length=50)
-    telefone = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11,
+                                validators=[MinLengthValidator(11)])
     grupo = models.ForeignKey(Grupo,
                               on_delete=models.CASCADE,
                               null=True,
                               related_name='participante')
-    codigo_match = models.IntegerField(max_length=6,
-                                       null=True)
+    codigo_match = models.IntegerField(null=True)
     
     def __str__(self):
         return self.nome
-    
